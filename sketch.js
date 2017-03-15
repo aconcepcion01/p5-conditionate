06d3332a9f06cbf5202f1a0e2753e7e3512948fe
amani concepcion <aconcepcion@wlapcs.org>
2017-03-15 19:31:06 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var x = 0;
+var speed = 3;
+
+
+function setup(){
+    createCanvas(600,400);
+}
+
+function draw(){
+  background(50);  
+  
+  //if i wrote this so background changes to pink when clicked 
+  if(mouseIsPressed){
+      background(255,0,250);
+  }
+  
+  //makes outline white, 6px thick, no color inside shapes 
+  stroke(225);
+  strokeWeight(6);
+  
+  if(mouseY<150){
+      fill(0,255,0);
+  }
+  else{
+      noFill();
+  }
+  
+  //a circle 
+  ellipse(x,200,100,100);
+  
+  //this code is going to get the circle to move on its own 
+  if(x>width || x<0){
+  console.log("CIRCLE IS OFF THE SCREEN!!!")
+  speed = speed * -1;
+  
+  }
+  
+  x = x + speed;
+  console.log(x);
+}
